e92b00eb182ebc01e6d50d5cc7653043d5b34d7a
Samuel Jacob <samueldotj@gmail.com>
2013-12-15 15:46:41 -0800

    Update Device.py
    Added GetAllDevice() and updated comments for all functions.

--- controllers/Device.py
+++ controllers/Device.py
@@ -1,12 +1,16 @@
 class Device:
+    """ Registers a device
     """
-    def RegisterDevice():
+    def RegisterDevice(DeviceType, Identification, Profile, Hub, Name, Appliance=None):
         raise
     
-    
-        
+    """ Fetches a device from database
+    """
     def GetDevice(DeviceID):
-        
         raise
         
-  
+    """ Fetches all devices associated with a user.
+        If DeviceType is provided then the result will be filtered to contain only that type of devices.
+    """
+    def GetAllDevice(Profile, DeviceType=None):
+        raise
