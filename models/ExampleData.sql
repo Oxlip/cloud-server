dab1bde55bde9b76157cb45252b29040fac3bff7
Samuel Jacob <samueldotj@gmail.com>
2013-12-28 14:08:02 -0800

    Added script to populate example data in the database.

---
+++ models/ExampleData.sql
@@ -0,0 +1,14 @@
+USE plugz;
+
+INSERT INTO profile(UserName, FirstName, LastName, DateOfBirth, Gender) VALUES('xyz', 'x', 'y', '1980-2-1', 1);
+
+INSERT INTO manufactureddevices(Identification, DeviceTypeId, DateOfManufacturing) VALUES('AABBCCDDEEF1', 1, '2013-1-1');
+INSERT INTO manufactureddevices(Identification, DeviceTypeId, DateOfManufacturing) VALUES('AABBCCDDEEG2', 2, '2013-1-2');
+INSERT INTO manufactureddevices(Identification, DeviceTypeId, DateOfManufacturing) VALUES('AABBCCDDEEG1', 2, '2013-1-2');
+INSERT INTO manufactureddevices(Identification, DeviceTypeId, DateOfManufacturing) VALUES('BBBBCCDDEEF1', 3, '2013-1-3');
+INSERT INTO manufactureddevices(Identification, DeviceTypeId, DateOfManufacturing) VALUES('BBBBCCDDEEF2', 3, '2013-1-4');
+INSERT INTO manufactureddevices(Identification, DeviceTypeId, DateOfManufacturing) VALUES('CCCCCCDDEEF1', 3, '2013-1-18');
+INSERT INTO manufactureddevices(Identification, DeviceTypeId, DateOfManufacturing) VALUES('CCCCCCDDEEF2', 3, '2013-1-23');
+INSERT INTO manufactureddevices(Identification, DeviceTypeId, DateOfManufacturing) VALUES('CCCCCCDDEEF3', 3, '2013-1-25');
+INSERT INTO manufactureddevices(Identification, DeviceTypeId, DateOfManufacturing) VALUES('CCCCCCDDEEF4', 3, '2013-1-25');
+
